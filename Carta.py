6980ad73bfa06784b83c0831f5fc48fa1f29d0a8
Daniel Rodrigues Ribeiro <daniel.rodrigues@g.globo>
2024-12-27 18:45:11 -0300

    classes porta, tesouro e carta corrigidas deixando os atrs privados

--- Carta.py
+++ Carta.py
@@ -3,9 +3,21 @@ from typing import Any
 
 
 class Carta(ABC):
-    def __init__(self, nome: str, image: str) -> None:
-        self.nome = nome
-        self.image = image
+    def __init__(self, nome: str, imagem: str) -> None:
+        self.__nome = nome
+        self.__imagem = imagem
+
+    def get_nome(self):
+        return self.__nome
+
+    def set_nome(self, nome: str):
+        self.__nome = nome
+
+    def get_imagem(self):
+        return self.__imagem
+
+    def set_imagem(self, imagem: str):
+        self.__imagem = imagem
 
     @abstractmethod
     def acao(self, parametro: Any) -> None:
